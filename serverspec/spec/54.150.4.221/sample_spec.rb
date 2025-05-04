5d281fd4c8cfd45dbbdf7b96081e4b4e9b769031
kubota-tk <wakuwaku4tel@gmail.com>
2025-05-04 12:36:22 +0900

    serverspecの書式修正

--- serverspec/spec/54.150.4.221/sample_spec.rb
+++ serverspec/spec/54.150.4.221/sample_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
 
 #gitのインストール確認
-describe package('git')do
+describe package('git') do
  it { should be_installed }
 end
 
 #22番ポートの疎通確認
-describe port(22)do
+describe port('22') do
  it { should be_listening }
 end
 
 #awsコマンド、s3確認ができるか
 describe command('sudo aws s3 ls') do
- it { should return_stdout /portfolio-ecs-picture/ }
+ its(:stdout) {should match /portfolio-ecs-picture/ }
 end
 
 #ECS Exec使用のため、SessionManagerPluginのインストール確認
 describe command('session-manager-plugin') do
- it { should return_stdout /The Session Manager plugin was installed successfully/ }
+ its(:stdout) {should match /The Session Manager plugin was installed successfully$/ }
 end
 
 #dockerが起動していることの確認
