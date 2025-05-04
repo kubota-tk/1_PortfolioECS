147bbef75e1c9979351e655b5f8cd87a894d44fe
kubota-tk <wakuwaku4tel@gmail.com>
2025-05-04 12:07:08 +0900

    serverspecのテスト内容更新

--- serverspec/spec/54.150.4.221/sample_spec.rb
+++ serverspec/spec/54.150.4.221/sample_spec.rb
@@ -5,5 +5,24 @@ describe package('git')do
  it { should be_installed }
 end
 
+#22番ポートの疎通確認
+describe port(80)do
+ it { should be_listening }
+end
 
+#awsコマンド、s3確認ができるか
+describe command('aws s3 ls') do
+  it { should return_stdout /portfolio-ecs-picture/ }
+end
+
+#ECS Exec使用のため、SessionManagerPluginのインストール確認
+describe command('session-manager-plugin') do
+  it { should return_stdout /The Session Manager plugin was installed successfully/ }
+end
+
+#dockerが起動していることの確認
+describe service('docker') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
 
